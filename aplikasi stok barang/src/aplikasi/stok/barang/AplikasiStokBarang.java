3da78bbfb2cef1376e29616b62c20ec135c9aa5b
Syahruladhim <117799089+Syahruladhim@users.noreply.github.com>
2023-10-30 09:48:28 +0700

    Add files via upload

---
+++ aplikasi stok barang/src/aplikasi/stok/barang/AplikasiStokBarang.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi.stok.barang;
+
+/**
+ *
+ * @author ARUL
+ */
+public class AplikasiStokBarang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
